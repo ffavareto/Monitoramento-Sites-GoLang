cb51f9c59335ad7c5058f6520ae95d53d304fb09
Lucas Santos <lucas.santos@faculdadeiv2.com.br>
2020-08-22 21:00:01 -0300

    verificação de comando do menu

--- hello.go
+++ hello.go
@@ -7,4 +7,11 @@ func main() {
 	versao := 1.1
 	fmt.Println("Olá Sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O valor da variável comando é:", comando)
 }
