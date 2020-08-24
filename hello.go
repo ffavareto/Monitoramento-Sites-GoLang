b8e7733f7f85bde02d79301327422f001268db45
Lucas Santos <lucas.santos@faculdadeiv2.com.br>
2020-08-23 22:19:39 -0300

    melhorando funcionalidade de monitoramento dos sites

--- hello.go
+++ hello.go
@@ -7,30 +7,34 @@ import (
 )
 
 func main() {
+
 	exibeIntroducao()
 
-	exibeMenu()
+	for {
 
-	comando := leComando()
+		exibeMenu()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Comando inválido")
-		os.Exit(-1)
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando inválido")
+			os.Exit(-1)
+		}
 	}
 
 }
 
 func exibeIntroducao() {
 	nome := "Lucas"
-	versao := 1.1
+	versao := 1.2
 	fmt.Println("Olá Sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
@@ -51,11 +55,20 @@ func leComando() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://faculdadeiv2.com.br"
+	sites := []string{"https://faculdadeiv2.com.br", "https://iv2.com.br", "https://class.faculdadeiv2.com.br/#/apps/student/courses/beginner/modules"}
+
+	for i, site := range sites {
+		fmt.Println("Testando site", i, ":", site)
+		testaSite(site)
+	}
+
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
 
 	if resp.StatusCode == 200 {
-		fmt.Println("O site", site, "foi carregado com sucesso!")
+		fmt.Println("O site", site, "está online!")
 	} else {
 		fmt.Println("O site", site, "está com problemas, StatusCode;", resp.StatusCode)
 	}
